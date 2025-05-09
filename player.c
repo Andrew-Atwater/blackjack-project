6784745650f303f537b0529b885e7966117a098e
Andrew-Atwater <andrew.atwater@maine.edu>
2025-05-09 14:22:57 -0400

    hand value with ace control implemented

--- player.c
+++ player.c
@@ -19,3 +19,31 @@ void free_hand(Hand *hand){
     hand->num_aces = 0;
 }
 
+void print_hand(const char *label, Hand *hand){
+    printf("%s's Hand: ", label);
+    for(int i = 0; i < hand->num_cards; i++){
+        printf("[%d] ", hand->cards[i]);
+    }
+    printf("\nHand Total: %d\n", hand->value);
+}
+
+void update_value(Hand *hand){
+    hand->value = 0;
+    hand->num_aces = 0;
+    
+    for(int i = 0; i < hand->num_cards; i++){
+        int current_value = hand->cards[i];
+        hand->value += current_value;
+        if(current_value == 11){
+            hand->num_aces += 1;
+        }
+    }
+    
+    for(int i = 0; i < hand->num_cards && hand->value > 21; i++){
+        if(hand->cards[i] = 11){
+            hand->cards[i] = 1;
+            hand->value -= 10;
+        }
+    }
+    
+}
