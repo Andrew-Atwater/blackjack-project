f903e31eb079cecb43872106f80127fb300bf358
Andrew-Atwater <andrew.atwater@maine.edu>
2025-05-10 13:20:44 -0400

    game logic first draft

--- blackjack.c
+++ blackjack.c
@@ -40,22 +40,57 @@ void play_round() {
         printf("Dealer's Hand: [%d] [??]\n", dealer_hand.cards[0]);
     }
 
+    //start the actual game
+    char choice;
+    while(1){
+        printf("Hit or stand? (h/s): ");
+        scanf("%c", &choice);
+        //hit logic
+        if(choice == 'h'){
+            deal_card(&player_hand, draw(&deck));
+            print_hand("Player", &player_hand);
+            if(player_hand.value > 21){
+                printf("Player busts!\n");
+                printf("Dealer's hand was: ");
+                print_hand("Dealer", &dealer_hand);
+                printf("\n");
+                break;
+            } else if(choice == 's') {
+                break;
+            } else {
+                printf("Invalid choice.\n");
+            }
+        }
+        if(player_hand.value <= 21){
+            print_hand("Dealer", &dealer_hand);
+            while(dealer_hand.value < 17){
+                deal_card(&dealer_hand, draw(&deck));
+                print_hand("Dealer", &dealer_hand);
+            }
+            if(dealer_hand.value > 21){
+                print_hand("Dealer", &dealer_hand);
+                printf("Dealer Busts! You win!\n");
+            } else {
+                if(player_hand.value > dealer_hand.value){
+                    print_hand("Player", &player_hand);
+                    print_hand("Dealer", &dealer_hand);
+                    printf("You Win!\n");
+                } else if(player_hand.value < dealer_hand.value){
+                    print_hand("Player", &player_hand);
+                    print_hand("Dealer", &dealer_hand);
+                    printf("Dealer Wins!\n")
+                } else {
+                    print_hand("Player", &player_hand);
+                    print_hand("Dealer", &dealer_hand);
+                    printf("Push.\n");
+                }
+            }
+        }
+    }
 }
 
-int blackjack (Hand *hand){
-    //handle blackjack
-}
-
-int bust (Hand *hand){
-    //handle bust
-}
-
-int compare(Hand *hand){
-    //handle dealer/player comparison and return accordingly
-}
-
-void log(const char *result, int bet, int net_change){
+/* void log(const char *result, int bet, int net_change){
     //handle write out of w/l
     FILE *log = fopen("session-log.txt", "a");
     fclose(log);
-}
+} */
